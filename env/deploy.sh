fd0d907aa92ded2aad82bde26ab627a20b7a2ee1
jgregorio <gregoriojesus0@gmail.com>
2021-04-16 16:33:21 +0200

    Improve pipeline using script deploy

---
+++ env/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /home/jgregorio/projects/handleticket-api/env
+
+mvn -f ../pom.xml clean package
+cp target/hadleticket-api-1.0.jar ./docker-handleticket-api/hadleticket-api.jar
+
+sudo docker container ls -a
+sudo docker container rm htapi
+
+sudo docker images
+sudo docker image rm handleticket-api:1.0
+
+sudo docker build -t handleticket-api:1.0 ./docker-handleticket-api/.
+
+sudo docker-compose -f ./docker-compose/docker-compose.yml up
